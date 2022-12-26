0f69095496fd7fb745e9d6211df20bfca0924d23
KostiaZ <zabolotnii.kostiantyn@gmail.com>
2022-12-26 21:04:21 +0000

    Deploying to gh-pages from @ KostiaZ/goit-js-hw-09@ba7300bab5d0ab81d178ec2c825df61b32576b74 🚀

---
+++ 01-color-switcher.5ae5069e.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),o=document.querySelector("button[data-start]");document.querySelector("button[data-stop");o.addEventListener("click",(function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}))}();
+//# sourceMappingURL=01-color-switcher.5ae5069e.js.map
--- 01-color-switcher.bc2ac7cb.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),o=document.querySelector("button[data-start]");document.querySelector("button[data-stop");function e(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}t.style.backgroundColor=e(),o.addEventListener("click",e);
-//# sourceMappingURL=01-color-switcher.bc2ac7cb.js.map

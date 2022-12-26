ee2320a8fe7733d9fc232217ac7338490c209f3d
Kostiantyn Zabolotnii <zabolotnii.kostiantyn@gmail.com>
2022-12-26 22:57:18 +0200

    add color switch at 1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,10 @@
+const body = document.querySelector('body');
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop');
 
+body.style.backgroundColor = getRandomHexColor();
+
+btnStart.addEventListener('click', getRandomHexColor);
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}

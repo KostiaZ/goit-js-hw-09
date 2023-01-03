e24c8017c58225bb6d80bffe121127a1f7b85495
Kostiantyn Zabolotnii <zabolotnii.kostiantyn@gmail.com>
2023-01-03 19:42:21 +0200

    3rd task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formBlock = document.querySelector('.form');
+
+formBlock.addEventListener('submit', event => {
+  event.preventDefault();
+  const { delay, step, amount } = event.currentTarget;
+  let currentDelay = Number(delay.value);
+  const stepNumber = Number(step.value);
+  const amountP = Number(amount.value);
+
+  for (let position = 1; position <= amountP; position++) {
+    createPromise(position, currentDelay);
+    currentDelay += stepNumber;
+  }
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+
+  promise
+    .then(({ position, delay }) => {
+      Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+    });
 }

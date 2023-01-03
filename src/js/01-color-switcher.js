68233744f82e9e950a4fe7a63a6e265e1065ec23
Kostiantyn Zabolotnii <zabolotnii.kostiantyn@gmail.com>
2023-01-03 14:41:26 +0200

    1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,12 +1,24 @@
-const body = document.querySelector('body');
+const bodyObject = document.querySelector('body');
 const btnStart = document.querySelector('button[data-start]');
-const btnStop = document.querySelector('button[data-stop');
+const btnStop = document.querySelector('button[data-stop]');
+let timer = 0;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-btnStart.addEventListener('click', onclick);
-function onclick() {
-  body.style.backgroundColor = getRandomHexColor();
-}
+btnStart.addEventListener('click', evt => {
+  evt.target.setAttribute('disabled', '');
+  btnStop.removeAttribute('disabled');
+
+  timer = setInterval(() => {
+    bodyObject.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+btnStop.addEventListener('click', evt => {
+  evt.target.setAttribute('disabled', '');
+  btnStart.removeAttribute('disabled');
+
+  clearInterval(timer);
+});
